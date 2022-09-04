bb79bd25028a6f3fe5d0580ade23c2ca6b114f0c
yusuke sugiyama <yus-sugiyama@uninosushi.com>
2022-09-04 14:12:56 +0900

    add storybook settings

---
+++ src/themes/index.ts
@@ -0,0 +1,13 @@
+import colors from "./colors";
+import fontSizes from "./fontSize";
+import letterSpacings from "./letterSpacings";
+import lineHeights from "./lineHeights";
+import space from "./space";
+
+export const theme = {
+  space,
+  fontSizes,
+  letterSpacings,
+  lineHeights,
+  colors,
+} as const;
