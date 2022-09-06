655e19862447b5be60edc814205940485ced1740
yusuke sugiyama <yus-sugiyama@uninosushi.com>
2022-09-06 23:43:55 +0900

    add molecules

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,10 @@
-import { addDecorator } from "@storybook/react"
-import { createGlobalStyle, ThemeProvider } from "styled-components"
-import { theme } from "../src/themes"
-import * as NextImage from "next/image"
-
+import { addDecorator } from '@storybook/react'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { theme } from '../src/themes'
+import * as NextImage from 'next/image'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
@@ -15,44 +14,40 @@ export const parameters = {
 }
 
 export const GlobalStyle = createGlobalStyle`
- html,
- body,
- textarea {
-  padding: 0;
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
- }
- * {
-  box-sizing: border-box;
- }
- a {
-  text-decoration: none;
-  transition: .25s;
-  color: #000000;
- }
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    transition: .25s;
+    color: #000000;
+  }
 `
 
 // Themeの適用
-addDecorator((story) => {
+addDecorator((story) => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
     {story()}
   </ThemeProvider>
-})
+))
 
 // next/imageの差し替え
 const OriginalNextImage = NextImage.default;
 
-Object.defineProperty(NextImage, "default", {
+Object.defineProperty(NextImage, 'default', {
   configurable: true,
-  value: (props) => typeof props.src === "string" ? (
+  value: (props) => typeof props.src === 'string' ? (
     <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
   ) : (
-      <OriginalNextImage {...props} unoptimized />
+    <OriginalNextImage {...props} unoptimized />
   ),
-})
-
-Object.defineProperty(NextImage, '__esModule', {
-  configurable: true,
-  value: true,
 })
---
+++ src/components/atoms/ShapeImage/index.tsx
@@ -0,0 +1,22 @@
+import Image, { ImageProps } from "next/image";
+import styled from "styled-components";
+
+type ImageShape = "circle" | "square";
+
+type ShapeImageProps = ImageProps & { shape?: ImageShape };
+
+// circleなら円形に
+const ImageWithShape = styled(Image)<{ shape?: ImageShape }>`
+  border-radius: ${({ shape }) => (shape === "circle" ? "50%" : "0")};
+`;
+
+/**
+ * シェイプイメージ
+ */
+const ShapeImage = (props: ShapeImageProps) => {
+  const { shape, ...imageProps } = props;
+
+  return <ImageWithShape shape={shape} {...imageProps} />;
+};
+
+export default ShapeImage;
