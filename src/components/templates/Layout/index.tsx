f50b210b22b7d128f421ce8479132b535742627e
yusuke sugiyama <yus-sugiyama@uninosushi.com>
2022-09-07 18:47:40 +0900

    fix templates

--- src/components/templates/Layout/index.tsx
+++ src/components/templates/Layout/index.tsx
@@ -1,9 +1,9 @@
-import Header from "components/organisms/Header";
-import Footer from "components/organisms/Footer";
-import Box from "components/layout/Box";
 import Separator from "components/atoms/Separator";
+import Box from "components/layout/Box";
+import Footer from "components/organisms/Footer";
+import Header from "components/organisms/Header";
 
-interface LayoutPorps {
+interface LayoutProps {
   children: React.ReactNode;
 }
 
