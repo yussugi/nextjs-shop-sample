86132ca7e1754055c658ce9a20d4ebda1bad9929
yussugi <yus-sugiyama@uninosushi.com>
2022-09-13 20:33:59 +0900

    fix image optimized

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   compiler: (() => {
     let compilerConfig = {
       // styledComponentsの有効化
